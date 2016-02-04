89026cb9d57f70fefadf63cc3a0de69a791d2f58
Wei Xie <wxie002@fiu.edu>
2016-02-03 21:10:14 -0800

    sss

---
+++ src/java_tutorial/EnumTest.java
@@ -0,0 +1,17 @@
+package java_tutorial;
+
+class FreshJuice {
+
+	   enum FreshJuiceSize{ SMALL, MEDIUM, LARGE }
+	   FreshJuiceSize size;
+	}
+
+
+	public class EnumTest {
+
+	   public static void main(String args[]){
+	      FreshJuice juice = new FreshJuice();
+	      juice.size = FreshJuice.FreshJuiceSize.MEDIUM ;
+	      System.out.println("Size: " + juice.size);
+	   }
+	}
